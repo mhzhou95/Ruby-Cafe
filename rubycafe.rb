19ec5b7fbca470571504583cdcd5d2a1a994f6d1
unknown <minghaozhou123@gmail.com>
2018-02-09 13:02:09 -0500

    email form added

--- rubycafe.rb
+++ rubycafe.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'sendgrid-ruby'
+include SendGrid
 
 get '/' do 
 	erb :home
@@ -19,3 +21,25 @@ end
 get '/contact' do
 	erb :contact
 end
+
+get '/thanks' do
+
+@email = params[:email]
+@subject = params[:subject]
+@content = params[:content]
+
+from = Email.new(email: @email)
+to = Email.new(email: 'minghaozhou123@gmail.com')
+subject = @subject
+content = Content.new(type: 'text/plain', value: @content)
+mail = Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+puts response.status_code
+puts response.body
+puts response.headers
+	erb :aftercontact
+end
+
+
